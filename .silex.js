6b36fc9370406d3edaad02fc82a30b78722408e3
Alex Hoyau <a.hoyau@silexlabs.org>
2024-08-09 15:27:28 +0200

    cleanup

--- .silex.js
+++ .silex.js
@@ -14,28 +14,6 @@ module.exports = async function (config) {
   await config.addPlugin(onboarding)
 
   initConnectors(config)
-  //config.setHostingConnectors([
-  //  new FtpConnector(config, {
-  //    type: ConnectorType.HOSTING,
-  //  }),
-  //  new DownloadPlugin(config),
-  //])
-
-  //config.setStorageConnectors([
-  //  new FtpConnector(config, {
-  //    type: ConnectorType.STORAGE,
-  //  }),
-  //  new GitlabConnector(config, {
-  //    clientId: process.env.GITLAB_CLIENT_ID,
-  //    clientSecret: process.env.GITLAB_CLIENT_SECRET,
-  //    domain: process.env.GITLAB_DOMAIN,
-  //  }),
-  //  new FramagitConnector(config, {
-  //    clientId: process.env.FRAMAGIT_CLIENT_ID,
-  //    clientSecret: process.env.FRAMAGIT_CLIENT_SECRET,
-  //    domain: process.env.FRAMAGIT_DOMAIN,
-  //  }),
-  //])
 
   // CMS Plugin
   config.addPlugin(StaticPlugin, {
@@ -66,131 +44,42 @@ module.exports = async function (config) {
 }
 
 const env = {
-  STORAGE_CONNECTORS: process.env.STORAGE_CONNECTORS || 'ftp,gitlab,gitlab2',
-  HOSTING_CONNECTORS: process.env.HOSTING_CONNECTORS || 'ftp,gitlab,gitlab2,download',
-  SILEX_FS_ROOT: process.env.SILEX_FS_ROOT || join(process.cwd(), '/silex/storage'),
-  SILEX_FS_HOSTING_ROOT: process.env.SILEX_FS_HOSTING_ROOT || join(process.cwd(), '/silex/hosting'),
-  GITLAB_DISPLAY_NAME: process.env.GITLAB_DISPLAY_NAME || 'Gitlab',
   GITLAB_CLIENT_ID: process.env.GITLAB_CLIENT_ID,
   GITLAB_CLIENT_SECRET: process.env.GITLAB_CLIENT_SECRET,
   GITLAB_DOMAIN: process.env.GITLAB_DOMAIN,
-  GITLAB2_DISPLAY_NAME: process.env.GITLAB2_DISPLAY_NAME || 'Gitlab',
-  GITLAB2_CLIENT_ID: process.env.GITLAB2_CLIENT_ID,
-  GITLAB2_CLIENT_SECRET: process.env.GITLAB2_CLIENT_SECRET,
-  GITLAB2_DOMAIN: process.env.GITLAB2_DOMAIN,
-  FTP_STORAGE_PATH: process.env.FTP_STORAGE_PATH || '/silex/storage',
-  FTP_HOSTING_PATH: process.env.FTP_HOSTING_PATH || '/silex/hosting',
 }
 
 // Create alternate versions of the the Gitlab connector
 class GitlabConnector1 extends GitlabConnector {
-  displayName = env.GITLAB_DISPLAY_NAME
-  constructor(config, options) {
-    super(config, options)
-  }
-}
-
-class GitlabConnector2 extends GitlabConnector {
-  connectorId = 'gitlab2'
-  displayName = env.GITLAB2_DISPLAY_NAME
+  displayName = 'Internet 2000'
+  icon = 'https://internet2000.net/cms/assets/362020c9-10d9-40db-8a92-0d4f96bd886b.png'
   constructor(config, options) {
     super(config, options)
   }
 }
 
 class GitlabHostingConnector1 extends GitlabHostingConnector {
-  displayName = env.GITLAB_DISPLAY_NAME
-  constructor(config, options) {
-    super(config, options)
-  }
-}
-
-class GitlabHostingConnector2 extends GitlabHostingConnector {
-  connectorId = 'gitlab2'
-  displayName = env.GITLAB2_DISPLAY_NAME
+  displayName = 'Internet 2000'
+  icon = 'https://internet2000.net/cms/assets/362020c9-10d9-40db-8a92-0d4f96bd886b.png'
   constructor(config, options) {
     super(config, options)
   }
 }
 
 function initConnectors(config) {
-  // Add storage and hosting connectors from env vars
-  if (env.STORAGE_CONNECTORS) {
-    config.setStorageConnectors([])
-    const connectors = env.STORAGE_CONNECTORS.split(',')
-    connectors.forEach((connector) => {
-      console.info('> Add storage connector from env var:', connector)
-      switch (connector) {
-        case 'fs':
-          config.addStorageConnector(new FsStorage(config, {
-            path: env.SILEX_FS_ROOT,
-          }))
-          break
-        case 'gitlab':
-          config.addStorageConnector(new GitlabConnector1(config, {
-            clientId: env.GITLAB_CLIENT_ID,
-            clientSecret: env.GITLAB_CLIENT_SECRET,
-            domain: env.GITLAB_DOMAIN,
-          }))
-          break
-        case 'gitlab2':
-          config.addStorageConnector(new GitlabConnector2(config, {
-            clientId: env.GITLAB2_CLIENT_ID,
-            clientSecret: env.GITLAB2_CLIENT_SECRET,
-            domain: env.GITLAB2_DOMAIN,
-          }))
-          break
-        case 'ftp':
-          config.addStorageConnector(new FtpConnector(config, {
-            type: ConnectorType.STORAGE,
-            path: env.FTP_STORAGE_PATH,
-          }))
-          break
-        default:
-          console.error('Unknown storage connector', connector)
-          throw new Error(`Unknown storage connector ${connector}`)
-      }
+  config.setStorageConnectors([
+    new GitlabConnector1(config, {
+      clientId: env.GITLAB_CLIENT_ID,
+      clientSecret: env.GITLAB_CLIENT_SECRET,
+      domain: env.GITLAB_DOMAIN,
     })
-  }
+  ])
 
-  if (env.HOSTING_CONNECTORS) {
-    config.setHostingConnectors([])
-    const connectors = env.HOSTING_CONNECTORS.split(',')
-    connectors.forEach((connector) => {
-      console.info('> Add hosting connector from env var:', connector)
-      switch (connector) {
-        case 'fs':
-          config.addHostingConnector(new FsHosting(config, {
-            path: env.SILEX_FS_HOSTING_ROOT,
-          }))
-          break
-        case 'gitlab':
-          config.addHostingConnector(new GitlabHostingConnector1(config, {
-            clientId: env.GITLAB_CLIENT_ID,
-            clientSecret: env.GITLAB_CLIENT_SECRET,
-            domain: env.GITLAB_DOMAIN,
-          }))
-          break
-        case 'gitlab2':
-          config.addHostingConnector(new GitlabHostingConnector2(config, {
-            clientId: env.GITLAB2_CLIENT_ID,
-            clientSecret: env.GITLAB2_CLIENT_SECRET,
-            domain: env.GITLAB2_DOMAIN,
-          }))
-          break
-        case 'ftp':
-          config.addHostingConnector(new FtpConnector(config, {
-            type: ConnectorType.HOSTING,
-            path: env.FTP_HOSTING_PATH,
-          }))
-          break
-        case 'download':
-          config.addHostingConnector(new DownloadConnector(config))
-          break
-        default:
-          console.error('Unknown hosting connector', connector)
-          throw new Error(`Unknown hosting connector ${connector}`)
-      }
+  config.setHostingConnectors([
+    new GitlabHostingConnector1(config, {
+      clientId: env.GITLAB_CLIENT_ID,
+      clientSecret: env.GITLAB_CLIENT_SECRET,
+      domain: env.GITLAB_DOMAIN,
     })
-  }
+  ])
 }
